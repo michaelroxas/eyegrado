ee71b4481aaf7adb17211877dcbc6260ed82e1bb
Michael Roxas <michaelroxas.od@gmail.com>
2022-01-10 13:38:57 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "records#index"
-  get "/records", to: "records#index"
+  root "patients#index"
+  get "/patients", to: "patients#index"
 end
